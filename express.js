545077921d571483d702635dcfafe3f11ae96e2b
Said Ravest (SaidTattoo) <said.tattoo@gmail.com>
2023-08-10 15:57:42 -0400

    desafio 2

--- express.js
+++ express.js
@@ -1,9 +1,36 @@
-// Ejercicio 2: crear servidor HTTP con Express
-function startServer () {
-  // const server = app.listen(...)
-  // return server
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>¡Hola mundo!</h1>')
+})
+
+app.get('/logo.webp', (req, res) => {
+  const filePath = path.join(__dirname, 'assets', 'logo.webp')
+  res.sendFile(filePath)
+})
+
+app.get('/404', (req, res) => {
+  res.status(404).send('<h1>404</h1>')
+})
+
+app.post('/contacto', (req, res) => {
+  res.status(201).json(req.body)
+})
+
+app.all('*', (req, res) => {
+  res.status(405).send('Method Not Allowed')
+})
+
+const startServer = (port = process.env.PORT || 1234) => {
+  const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
+
+  return server
 }
 
-module.exports = {
-  startServer
-}
+module.exports = { startServer }
--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-// Ejercicio 1: crear servidor HTTP con Node
+const http = require('http')
+const fs = require('fs')
+
+const PORT = process.env.PORT || 1234
+
 function startServer () {
-  // return server
+  const server = http.createServer((req, res) => {
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`)
+
+    if (req.method === 'GET') {
+      switch (parsedUrl.pathname) {
+        case '/':
+          res.setHeader('Content-Type', 'text/html')
+          res.write('<h1>¡Hola mundo!</h1>')
+          res.end()
+          break
+        case '/logo.webp':
+          fs.readFile('./assets/logo.webp', (err, data) => {
+            if (err) {
+              res.writeHead(500)
+              res.end()
+            } else {
+              res.writeHead(200, { 'Content-Type': 'image/webp' })
+              res.end(data)
+            }
+          })
+          break
+        case '/404':
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.write('<h1>404</h1>')
+          res.end()
+          break
+        default:
+          res.writeHead(404)
+          res.end()
+      }
+    } else if (req.method === 'POST' && parsedUrl.pathname === '/contacto') {
+      let body = ''
+
+      req.on('data', chunk => {
+        body += chunk.toString()
+      })
+
+      req.on('end', () => {
+        const parsedBody = JSON.parse(body)
+        res.writeHead(201, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(parsedBody))
+      })
+    } else {
+      res.writeHead(405)
+      res.end()
+    }
+  })
+
+  server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+  })
+
+  return server
 }
 
 module.exports = {
